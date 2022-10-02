3aec8665510481aae773be708abc3d2d0933c83a
Aaron Griffin <Aaron@Aarons-Macbook-Air.local>
2022-10-02 19:04:10 +0100

    starting to try and get the app working with the jni

--- app/src/main/java/java_packet_sniffer/Sniffer.java
+++ app/src/main/java/java_packet_sniffer/Sniffer.java
@@ -1,5 +1,6 @@
 package java_packet_sniffer;
 
 public class Sniffer {
-    
+    static {System.loadLibrary("packetsniffer");}
+    public static native void packetSniffer(int argc, char[][] argv);
 }
